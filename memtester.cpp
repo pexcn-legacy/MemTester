2dd72756fa00f77f67dd0ead2d9ddb976f4f2352
Max Witten <pexcn97@gmail.com>
2017-08-24 11:39:03 +0800

    Update code style.

--- memtester.cpp
+++ memtester.cpp
@@ -14,17 +14,19 @@ using namespace std;
 
 int main()
 {
- cout<<"I will try my best to fill your RAM."<<endl
- <<"You have 3 seconds to quit (Ctrl+C)"<<endl;
- sleep(3);
- long allocatedMB = 0;
- while (true)
- {
- unsigned char * leakThisMemoryPlease = new unsigned char[10485760];
- for (int i = 0; i < 10485760; i++)
- leakThisMemoryPlease[i] = i*rand();
- allocatedMB += 10;
- cout<<allocatedMB<<"MB allocated"<<endl;
- }
- return 0;
+    cout << "I will try my best to fill your RAM." << endl
+         << "You have 3 seconds to quit (Ctrl+C)" << endl;
+    sleep(3);
+    long allocatedMB = 0;
+
+    while (true)
+    {
+        unsigned char *leakThisMemoryPlease = new unsigned char[10485760];
+        for (int i = 0; i < 10485760; i++)
+            leakThisMemoryPlease[i] = i * rand();
+        allocatedMB += 10;
+        cout << allocatedMB << "MB allocated" << endl;
+    }
+
+    return 0;
 }
